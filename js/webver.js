6a03b9cc53db5be93ecde4ab48bb2453fabb5c3b
Muhammad Fachri Al Husein <110403891+Flixdz@users.noreply.github.com>
2024-07-07 11:33:00 +0700

    Add files via upload

--- js/webver.js
+++ js/webver.js
@@ -1,2 +1,2 @@
-var websiteVersion = "Flixdz @flixpseal 2024 v3.3 r-5"
-var websiteDate = "Pembaruan Terakhir 24 Maret 2023"
+var websiteVersion = "Flixdz @flixpseal 2024 v3.7"
+var websiteDate = "Pembaruan Terakhir 07 April 2024"
